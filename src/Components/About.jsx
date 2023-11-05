8d6373321dd0342e06ac8d60626a24672bf7e946
laxmi <lkc712628@gmail.com>
2023-11-05 14:28:11 +0000

    created about page

---
+++ src/Components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const About = () => (
+	<main role="main">
+		<div>
+			<h1>Greenwich Mums</h1>
+			<img
+			  className = "loginBg"
+			  src="/public/study.jpg"
+			  width="500"
+			  height="350"
+			  alt="greenwich-mums"
+			></img>
+			
+		</div>
+	</main>
+);
+
+export default About;
